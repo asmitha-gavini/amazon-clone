7a5cc3d54241ba6574b9f05c723669f87b30ab11
asmitha-gavini <asmithagavini@gmail.com>
2026-09-23 12:10:07 +0530

    added files

---
+++ script.js
@@ -0,0 +1,23 @@
+function search() {
+    alert("Searching Amazon...");
+}
+
+function login() {
+    alert("Login / Sign in");
+}
+
+function cart() {
+    alert("Your cart is empty");
+}
+
+function menu() {
+    alert("Amazon Menu");
+}
+
+function shop() {
+    alert("Let's start shopping!");
+}
+
+function viewProduct() {
+    alert("Product page coming soon!");
+}
